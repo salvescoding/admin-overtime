01ae94ed9dde39b1541e81919d5842db6611dd60
Sergio Alves <salves2383@gmail.com>
2018-05-21 19:38:55 +0200

    Added new form for post and creation of new post with TDD

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,5 +15,23 @@ describe 'Posts' do
         expect(page).to have_content(/Posts/)
       end
     end
+
+    describe 'new' do
+      it '200 OK' do
+        visit new_post_path
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'create new post with form' do
+        visit new_post_path
+
+        fill_in 'post[date]', with: Date.today
+        fill_in 'post[rationale]', with: 'Star Wars'
+
+        click_on "Save"
+
+        expect(page).to have_content(/Star Wars/)
+      end
+    end
   end
 end
