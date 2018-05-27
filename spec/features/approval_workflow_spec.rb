dd25895484cdd662aada42d7b406e8dcf05c210c
Sergio Alves <salves2383@gmail.com>
2018-05-27 20:12:14 +0200

    Implemented badges for status on posts, implemented permission for when posts have been aproved by admin

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -1,25 +1,32 @@
 require 'rails_helper'
 
 describe 'Approval workflow' do
+  let(:admin_user) { create(:admin_user) }
+  let(:user) { create(:user) }
+  let(:approval_post) { create(:post, user_id: user.id) }
   before do
-    @admin_user = create(:admin_user)
-    login_as(@user, scope: :user)
+    login_as(admin_user, scope: :user)
   end
 
   describe 'edit' do
-    before do
-      @post = create(:post)
-      visit edit_post_path(@post)
-    end
 
     it 'normal user have no status options' do
-      logout(:user)
-      user = create(:user)
+      logout(:admin_user)
       login_as(user, scope: :user)
       post = create(:post, user_id: user.id)
 
       visit edit_post_path(post)
       expect(page).to_not have_content(/Approved|Rejected|Submitted/)
+    end
+
+    it 'should not be editable after admin_user approval' do
+      approval_post.approved!
+      logout(:admin_user)
+      login_as(user, scope: :user)
+
+      visit edit_post_path(approval_post)
+
+      expect(current_path).to eq(root_path)
     end
 
   end
