ad0a646a6ae9e8d44126bee48f2f9c80724e302d
Sergio Alves <salves2383@gmail.com>
2018-05-29 19:09:56 +0200

    Integrated validations and defaults for audit logs attrs

--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,17 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  let(:audit_log) { create(:audit_log)  }
+  let(:audit) { create(:audit_log)  }
+  let(:user)  { create(:user) }
   describe 'creation' do
     it 'can be created with valid data' do
-      audit = create(:audit_log)
       expect(audit).to be_valid
     end
   end
 
   describe 'validations' do
     it 'should not be valid without start date' do
+      audit.start_date = nil
+      expect(audit).to_not be_valid
+    end
 
+    it 'should not be valid without status' do
+      audit.status = nil
+      expect(audit).to_not be_valid
+    end
+
+    it 'should have a start date with 6 days prior' do
+      audit = AuditLog.create!(user_id: user.id)
+      expect(audit.start_date).to eq(Date.today - 6.days)
     end
 
   end
