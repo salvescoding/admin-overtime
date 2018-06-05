9146462db9c1ca3ea1cbde93cedf5a9332f5000e
Sergio Alves <salves2383@gmail.com>
2018-06-05 18:35:06 +0200

    Block non admin users to access audit logs

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -7,6 +7,7 @@ describe 'Describe the audit log features' do
     before do
       admin_user = create(:admin_user)
       login_as(admin_user, scope: :user)
+      create(:audit_log)
     end
     it  'have an index page that can be reached' do
       visit audit_logs_path
@@ -14,9 +15,16 @@ describe 'Describe the audit log features' do
     end
 
     it 'renders audit log content' do
-      create(:audit_log)
       visit audit_logs_path
       expect(page).to have_content("Sergio Alves")
+    end
+
+    it 'non admin users cannot access audit log index page' do
+      logout(:user)
+      user = create(:user)
+      login_as(user, scope: :user)
+      visit audit_logs_path
+      expect(current_path).to eq(root_path)
     end
   end
 end
