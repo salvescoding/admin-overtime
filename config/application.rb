711ab2aafeeb782e21275876f32111133bee8793
Sergio Alves <salves2383@gmail.com>
2018-06-14 20:15:02 +0200

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,8 @@ module OvertimeApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
 
     config.generators do |g|
