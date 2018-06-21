69d049289a5141316e91b41695ca3ba7414bb137
Sergio Alves <salves2383@gmail.com>
2018-06-21 17:01:19 +0200

    Created an employee model using single table inheritance

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 Post.delete_all
 AuditLog.delete_all
 User.delete_all
+Employee.delete_all
 AdminUser.delete_all
 puts "All posts deleted"
 puts "All users deleted"
@@ -13,7 +14,7 @@ AdminUser.create!(email: "admin@user.com",
                   password_confirmation: "password",
                   phone: "555938395"
                   )
-@user = User.create!(email: "user@user.com",
+@employee = Employee.create!(email: "user@user.com",
                      first_name: "Marise",
                      last_name: "Kamp",
                      password: "password",
@@ -26,13 +27,13 @@ puts "admin user and users created"
 reasons = ["I have to work a lot", "They broke everything and I have to fix it now"]
 
 12.times do
-  Post.create!(date: Date.today, rationale: reasons.sample, user_id: @user.id, overtime_request: [2.0, 4.7, 1.5, 20.0].sample)
+  Post.create!(date: Date.today, rationale: reasons.sample, user_id: @employee.id, overtime_request: [2.0, 4.7, 1.5, 20.0].sample)
 end
 
 puts "100 posts have been created"
 
-AuditLog.create!(user_id: @user.id,  status: 0, start_date: Date.today - 6.days)
-AuditLog.create!(user_id: @user.id,  status: 0, start_date: Date.today - 13.days)
-AuditLog.create!(user_id: @user.id,  status: 0, start_date: Date.today - 20.days)
+AuditLog.create!(user_id: @employee.id,  status: 0, start_date: Date.today - 6.days)
+AuditLog.create!(user_id: @employee.id,  status: 0, start_date: Date.today - 13.days)
+AuditLog.create!(user_id: @employee.id,  status: 0, start_date: Date.today - 20.days)
 
 puts "3 audit logs created"
